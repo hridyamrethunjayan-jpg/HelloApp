f738be499f645661f93209ce97c5e540d952c044
hridyamrethunjayan-jpg <hridyamrethunjayan@gmail.com>
2026-03-18 15:45:47 +0530

    UC2 display hello name

---
+++ src/main/java/org/example/HelloAppName.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class HelloAppName {
+    public static void main(String[] args){
+        String name=args[0];
+        System.out.println("Hello "+name+"!");
+    }
+}
